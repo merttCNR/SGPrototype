db3dbcbf12d34580c42c53a82f0bc4f60147d037
Mert Çınar <kadirmertcnr@gmail.com>
2023-04-06 15:59:22 +0300

    Organised

--- SnakeGame/Assets/Scripts/Player/CoinDetector.cs
+++ SnakeGame/Assets/Scripts/Player/CoinDetector.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 public class CoinDetector : MonoBehaviour
 {
    public TailManager tailManager;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Coin")){
             Debug.Log("çalışıyor!!!");
--- SnakeGame/Assets/Scripts/Player/TailManager.cs
+++ SnakeGame/Assets/Scripts/Player/TailManager.cs
@@ -4,10 +4,12 @@ public class TailManager : MonoBehaviour
 {
     [Header("Int")]
     int tailLength = 1;
+    
     [Header("GameObjects")]
     public GameObject coinPreb;
     public GameObject collector;
     public Transform holder;
+
     public void TailFunc(){
         for (int i = 0; i < tailLength; i++)
         {
